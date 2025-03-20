894e41e2755f1c3d4e8b079d7db0ea7a64f62e47
young-win <young.zhu.2012@gmail.com>
2025-03-20 13:48:48 +0800

    日报完成

--- edspy/eds_reportor.py
+++ edspy/eds_reportor.py
@@ -51,7 +51,79 @@ class EdsReportor:
             time.sleep(2)
 
             # 开始填日报
-            self._daily_log()
+            self._daily_report()
+
+    def _daily_report(self):
+        # print(f'日报内容：{self.logContent.daily()}')
+        self.driver.get(self.settings.daily_url)
+
+        # 填7天
+        report_date = date.today()
+        # report_date = date(2025, 3, 3)
+        for _ in range(7):
+            self._do_daily_report(report_date)
+            time.sleep(1)
+
+            # 当天日报完成，NEXT
+            self.driver.find_element(By.ID, "btnnext").click()
+            report_date = report_date + timedelta(days=1)
+    
+    def _do_daily_log(self, report_date):
+        """填日报"""
+        if self._should_report(report_date):
+            memo = self.work_report.daily_work_report()
+
+            # 页面上点2次【确定】，即可完成一天的日志
+            # 上午
+            # self.driver.find_element(By.ID, "txtMemo").send_keys(memo)
+            # self.driver.find_element(By.ID, "btnSave").click()
+            # 下午
+            # self.driver.find_element(By.ID, "txtMemo").send_keys(memo)
+            # self.driver.find_element(By.ID, "btnSave").click()
+            for member in AMPM:
+                btn_save = WebDriverWait(self.driver, self.settings.timeout).until(
+                    EC.presence_of_element_located((By.ID, "btnSave"))
+                )
+
+                try:
+                    self.driver.find_element(By.ID, "txtMemo").send_keys(memo)
+                    # self.driver.find_element(By.ID, "btnSave").click()
+                    btn_save.click()
+
+                except Exception as e:
+                    _logger.error(f"日报{report_date} {member.value} 出现错误：{e}")
+                
+                time.sleep(1)
+
+            _logger.info(f"{report_date} - 日报完成")
+
+    def _should_report(self, report_date):
+        """ {report_date} 这天是否还需要填写日报"""
+        if calendar.is_workday(report_date):
+            # 开始时间 
+            # 还未填日志的，有值；已经填过的，值为空
+            retries = 3
+            for i in range(retries):
+                try:
+                    start_time = WebDriverWait(self.driver, self.settings.timeout).until(
+                        EC.presence_of_element_located((By.ID, "txtStartTime"))
+                    )
+                    break
+                except StaleElementReferenceException:
+                    _logger.info(f"retry {retries}...")
+                    if i == retries - 1:
+                        raise
+            start_time_value = start_time.get_attribute("value")
+            # print(f"{report_date}: {start_time_value}")
+            
+            if start_time_value:
+                return True
+            else:
+                print(f"{report_date} - 已填写")
+                return False
+        else:
+            print(f"{report_date} - 休息")
+            return False
 
     def _weekly_report(self):
         """填周报"""
@@ -133,3 +205,9 @@ class EdsReportor:
         self.driver = webdriver.Chrome(options=options, service=service) 
         # 不用 driver 好像也可以啊
         # self.driver = webdriver.Chrome(options=options) 
+
+from enum import Enum
+
+class AMPM(Enum):
+    AM = "AM"  # 显式赋值
+    PM = "PM"  # 显式赋值
